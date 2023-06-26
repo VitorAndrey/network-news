087d3837a1147f5ddb877e204cce799e102358f6
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-06-25 23:10:47 -0300

    adicionei link na home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/newsletters">Newsletters</Link>
+    </>
+  );
 }
